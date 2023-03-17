3d4f14831fc7e14bdcb759d468b6d0e654e0751c
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-03-17 18:35:18 +0530

    Typechain added through npm and watchtower -> oracle

--- src/watchtower/contracts.ts
+++
@@ -1,6 +0,0 @@
-export const oracleAbi = [
-  // Fetching data
-  // Submitting data
-  "function submit_SentinelData(uint256,uint256,uint256,uint256,uint256)",
-  "function submit_PledgingStatus(uint256,address,uint256)",
-];
